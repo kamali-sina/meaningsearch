82dadda1ec96a6c74370204b476d72f1a2bc4909
sinku <sininoir@gmail.com>
2023-06-22 10:51:33 +0330

    Add pipe-supporting input reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{self, BufRead};
+
+const WORDLIST:&str = include_str!("./meaningfullwordlist.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let input = get_input();
+    let lines = input.split('\n');
+    let meaningfullwords = WORDLIST.split('\n');
+
+    for line in lines {
+        println!("{line}");
+    }
 }
+
+fn get_input() -> String {
+    let input = io::stdin().lock().lines().fold("".to_string(), |acc, line| {
+        acc + &line.unwrap() + "\n"
+    });
+    return input;
+}
+
+
+// if output_in_binary.find(stop_word_in_binary.as_str()) == None {
+//     _error("Could not find an embedded message in the image");
+//     exit(1);
+// }
+// output_in_binary = output_in_binary[..output_in_binary.find(stop_word_in_binary.as_str()).unwrap()].to_string();
